747f70ae65b296c2d4d5e606b47797b4f8dd580f
sheikh-abubakar <abubakarjavaid034@gmail.com>
2025-07-05 15:48:05 +0500

    React Mega Poject for Blog posting starated

---
+++ React/React-Projects/ReactMegaProject/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+console.log(import.meta.env.VITE_APPWRITE_URL)
+  return (
+    <>
+      <h1 className='bg-yellow-400'>hello</h1>
+    </>
+  )
+}
+
+export default App
